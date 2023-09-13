809dcba078b7191a42e4ea8321da135d407fcf4c
Marcus <marc303i@stud.kea.dk>
2023-09-13 15:55:39 +0200

    Har indført så et FootballTeams kan få et Defense og Offense der er altid mellem 1 eller 10 og kan være random mellem det, hvis du ikke giver et defence og offence

--- FootballAppNew/FootballTeams.cs
+++ FootballAppNew/FootballTeams.cs
@@ -7,6 +7,9 @@ public class FootballTeams
     public string FullClubName { get; set; }
     public string Abbreviation { get; set; }
     public char SpecialRanking { get; set; }
+    
+    public int Defense { get; set; }
+    public int Offense { get; set; }
 
     // Constructor
     public FootballTeams(string fullClubName, string abbreviation, char specialRanking)
@@ -14,7 +17,16 @@ public class FootballTeams
         FullClubName = fullClubName;
         Abbreviation = abbreviation;
         SpecialRanking = specialRanking;
-        
+        Defense = RollRandomStat();
+        Offense = RollRandomStat();
+    }
+    public FootballTeams(string fullClubName, string abbreviation, char specialRanking, int defence, int offense)
+    {
+        FullClubName = fullClubName;
+        Abbreviation = abbreviation;
+        SpecialRanking = specialRanking;
+        Defense = LimitStat(defence);
+        Offense = LimitStat(offense);
     }
 
     public FootballTeams(string[] teamValues)
@@ -22,11 +34,28 @@ public class FootballTeams
         FullClubName = teamValues[0];
         Abbreviation = teamValues[1];
         SpecialRanking = teamValues[2][0];
+        Defense = Convert.ToInt32(teamValues[3]);
+        Offense = Convert.ToInt32(teamValues[4]);
     }
 
     public string ConvertTOCSVFormat()
     {
-        return $"{FullClubName},{Abbreviation},{SpecialRanking}";
+        return $"{FullClubName},{Abbreviation},{SpecialRanking},{Defense},{Offense}";
+    }
+    
+    private int LimitStat(int stat)
+    {
+        if (stat > 10) { return 10; }
+         if (stat < 1) { return 1; } 
+         return stat;
+    }
+
+    private int RollRandomStat()
+    {
+        int maxStat = 10;
+        Random random = new Random();
+        int randomStat = random.Next(maxStat) + 1;
+        return randomStat;
     }
     
 }
